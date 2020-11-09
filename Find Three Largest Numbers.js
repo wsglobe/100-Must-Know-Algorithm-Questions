360d368b42f366495c19ec107efe861c6956d629
Walter Wang <wsglobe@gmail.com>
2020-11-09 17:07:21 -0600

    s

--- Find Three Largest Numbers.js
+++ Find Three Largest Numbers.js
@@ -3,3 +3,35 @@
 // array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
 // Sample Output
 // [18, 141, 541]
+// Can we just sort the input array and return the last three elements in the sorted array?
+
+// Solution
+function findThreeLargestNumbers(array) {
+  const threeLargest = [null, null, null];
+  for (const num of array) {
+    updateLargest(threeLargest, num);
+  }
+  return threeLargest;
+}
+
+function updateLargest(threeLargest, num) {
+  if (threeLargest[2] === null || num > threeLargest[2]) {
+    shiftAndUpdate(threeLargest, num, 2);
+  } else if (threeLargest[1] === null || num > threeLargest[1]) {
+    shiftAndUpdate(threeLargest, num, 1);
+  } else if (threeLargest[0] === null || num > threeLargest[0]) {
+    shiftAndUpdate(threeLargest, num, 0);
+  }
+}
+
+function shiftAndUpdate(array, num, idx) {
+  for (let i = 0; i <= idx; i++) {
+    if (i === idx) {
+      array[i] = num;
+    } else {
+      array[i] = array[i + 1];
+    }
+  }
+}
+
+exports.findThreeLargestNumbers = findThreeLargestNumbers;
