199c8624294fe1c28a75b8d871b6b319a5fb643e
Walter Wang <wsglobe@gmail.com>
2020-11-09 00:35:02 -0600

    hints

---
+++ Binary Search.js
@@ -0,0 +1,4 @@
+// Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1.
+// Sample Input [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+// Target = 33
+// Sample Output 3
