1b38579a51c9b77b0279a0ce1a1bd4174e478961
Walter Wang <wsglobe@gmail.com>
2020-11-08 22:51:35 -0600

    solution

--- Product Sum.js
+++ Product Sum.js
@@ -2,3 +2,18 @@
 // Sample Input
 // array = [5, 2, [7, -1], 3, [6, [-13, 8]], 4]
 // Output 12 = 5 + 2 + 2 *(7 - 1) + 3 + 2 * (6 + 3 * ( -13 + 8) + 4)
+
+// Solution
+function productSum(array, multiplier = 1) {
+  let sum = 0;
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      sum += productSum(element, multiplier + 1);
+    } else {
+      sum += element;
+    }
+    return sum * multiplier;
+  }
+}
+
+exports.productSum = productSum;
