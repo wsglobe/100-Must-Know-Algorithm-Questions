844c4c55854c5ff480053e527f3dd02fca79f0fb
Walter Wang <wsglobe@gmail.com>
2020-11-09 01:16:51 -0600

    s2

--- Binary Search.js
+++ Binary Search.js
@@ -22,3 +22,26 @@ function binarySearchHelper(array, target, left, right) {
 }
 
 exports.binarySearch = binarySearch;
+
+
+// Solution 2
+function binarySearch(array, target) {
+  return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    const potentialMatch = array[middle];
+    if (target === potentialMatch) {
+      return middle;
+    } else if (target < potentialMatch) {
+      right = middle - 1;
+    } else {
+      left = middel + 1;
+    }
+  }
+  return -1;
+}
+
+exports.binarySearch = binarySearch;
