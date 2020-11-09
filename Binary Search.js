bfcd019b3d6cc8e730e97365a573845ac0ef520f
Walter Wang <wsglobe@gmail.com>
2020-11-09 01:11:59 -0600

    s1

--- Binary Search.js
+++ Binary Search.js
@@ -2,3 +2,23 @@
 // Sample Input [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
 // Target = 33
 // Sample Output 3
+
+// Solution 1
+function binarySearch(array, target) {
+  return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+  if (left > right) return -1;
+  const middle = Math.floor((left + right) / 2);
+  const potentialMatch = array[middle];
+  if (target === potentialMatch) {
+    return middle;
+  } else if (target < potentialMatch) {
+    return binarySearchHelper(array, target, left, middle - 1);
+  } else {
+    return binarySearchHelper(array, target, middle + 1, right);
+  }
+}
+
+exports.binarySearch = binarySearch;
