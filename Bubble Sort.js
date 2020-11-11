158a6928e7b88ec6282ddf9052d6839d45385808
Walter Wang <wsglobe@gmail.com>
2020-11-10 18:32:05 -0600

    sample

--- Bubble Sort.js
+++ Bubble Sort.js
@@ -1,1 +1,3 @@
 // Write a function that takes in an array of integers and returns a sorted version of that array. Use the Bubble Sort algorithm to sort the array.
+// array = [8, 5, 2, 9, 5, 6, 3]
+// sorted array = [2, 3, 5, 5, 6, 8, 9]
