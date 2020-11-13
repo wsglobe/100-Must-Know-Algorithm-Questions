83f43948ebf63d4480ebe69dfff335f546a2abfc
Walter Wang <wsglobe@gmail.com>
2020-11-12 23:34:02 -0600

    des

---
+++ Insertion Sort.js
@@ -0,0 +1,5 @@
+/* Write a function that takes in an array of integers and returns a sorted version of that array. Use the Insertion Sort algorithm to sort the array.
+Sample Input:
+array = [8, 5, 2, 9, 5, 6, 3]
+Sample Output:
+[2, 3, 5, 5, 6, 8, 9] */
