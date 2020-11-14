30c170b4ccd90cc055d5c72bfa56295e4e3e4e2e
Walter Wang <wsglobe@gmail.com>
2020-11-13 23:26:18 -0600

    solution

--- Insertion Sort.js
+++ Insertion Sort.js
@@ -1,5 +1,29 @@
 /* Write a function that takes in an array of integers and returns a sorted version of that array. Use the Insertion Sort algorithm to sort the array.
+
+Hint: Divide the input array into two subarrays in place. The first subarray should be sorted at all times and should start with a length of 1, while the second subarray should be unsorted. Iterate through the unsorted subarray, inserting all of its elements into the sorted subarray in the correct position by swapping them into place. Eventually, the entire array will be sorted.
+
 Sample Input:
 array = [8, 5, 2, 9, 5, 6, 3]
 Sample Output:
 [2, 3, 5, 5, 6, 8, 9] */
+
+// Solution
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      swap(j, j - 1, array);
+      j -= 1;
+    }
+  }
+  return array;
+}
+
+function swap(i, j, array) {
+  const temp = array[j];
+  array[i] = array[j];
+  array[i] = temp;
+}
+
+exports.insertionSort = insertionSort;
