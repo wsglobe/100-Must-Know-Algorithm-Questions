b8f4a1e9a4172bb2e0a7bc701ed561497be487bd
Walter Wang <wsglobe@gmail.com>
2020-11-14 18:52:20 -0600

    solution

--- Palindrome Check.js
+++ Palindrome Check.js
@@ -2,3 +2,19 @@
 A palidrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
 Sample Input:
 string = "abcdcba"; True */
+// Solution
+// O(n) time | O(1) space
+
+function isPalindrome(string) {
+  let leftIdx = 0;
+  let rightIdx = string.length - 1;
+  while (leftIdx < rightIdx) {
+    if (string[leftIdx] !== string[rightIdx])
+    return false;
+    leftIdx++;
+    rightIdx--;
+  }
+  return true;
+}
+
+exports.isPalindrome = isPalindrome;
