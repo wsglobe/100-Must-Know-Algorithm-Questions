1d5f1f4b843fb7f962261c5e2ade8a8982107900
Walter Wang <wsglobe@gmail.com>
2020-11-14 21:21:49 -0600

    S2

--- Caesar Cipher Encryptor.js
+++ Caesar Cipher Encryptor.js
@@ -29,3 +29,19 @@ exports.caesarCipherEncryptor = caesarCipherEncryptor;
 
 // Solution 2
 // O(n) time | O(n) space
+function caesarCipherEncryptor(string, key) {
+  const newLetters = [];
+  const newKey = key % 26;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for (const letter of string) {
+    newLetters.push(getNewLetter(letter, newKey, alphabet));
+  }
+  return newLetters.join('');
+}
+
+function getNewLetter(letter, key, alphabet) {
+  const newLetterCode = alphabet.indexOf(letter) + key;
+  return alphabet[newLetterCode % 26];
+}
+
+exports.caesarCipherEncryptor = caesarCipherEncryptor;
