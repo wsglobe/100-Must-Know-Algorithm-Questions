e6c99efec3343b9fe0f5926d8d373b794f6f566f
Walter Wang <wsglobe@gmail.com>
2020-11-14 18:16:38 -0600

    des

---
+++ Palindrome Check.js
@@ -0,0 +1,4 @@
+/* Write a function that takes in a non-empty string and returns a boolean representing whether the string is a palindrome.
+A palidrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
+Sample Input:
+string = "abcdcba"; True */
