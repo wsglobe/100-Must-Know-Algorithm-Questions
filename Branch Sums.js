899188662a3a10cffaec69b69b37c7ba1d312d5d
Walter Wang <wsglobe@gmail.com>
2020-11-15 22:00:42 -0600

    des

--- Branch Sums.js
+++ Branch Sums.js
@@ -1,3 +1,8 @@
+/* Write a function that takes in a Binary Tree and returns a list of its branch sums ordered from leftmost branch sum to rightmost branch sum.
+
+A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of nodes in a tree that starts at the root node and ends at any leaf node.
+
+Each BinaryTree node has an integer value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None / null.
 //Solution
 
 class BinaryTree {
