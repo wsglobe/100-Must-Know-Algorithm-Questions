ee3272fd9e87568d1a496ae51b89b52d2e302c18
Walter Wang <wsglobe@gmail.com>
2020-11-15 22:02:56 -0600

    optimal complex

--- Branch Sums.js
+++ Branch Sums.js
@@ -3,6 +3,10 @@
 A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of nodes in a tree that starts at the root node and ends at any leaf node.
 
 Each BinaryTree node has an integer value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None / null.
+
+*Optimal Space & Time Complexity
+O(n) time | O(n) space - where n is the number of nodes in the Binary Tree
+
 //Solution
 
 class BinaryTree {
