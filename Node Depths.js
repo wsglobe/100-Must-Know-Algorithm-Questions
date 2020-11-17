5cd780ddc2d29376ec1a1275dfd053608751a678
Walter Wang <wsglobe@gmail.com>
2020-11-16 19:43:07 -0600

    des

--- Node Depths.js
+++ Node Depths.js
@@ -1,6 +1,11 @@
-//The distance between a node in a Binary Tree and the tree's root is called the node's depths.
-//The depth of any node in the tree is equal to the depth of its parent node plus 1.
-//Write a function that takes in a Binary Tree and returns the sum of its node's depths.
+/*The distance between a node in a Binary Tree and the tree's root is called the node's depths.
+
+Note that The depth of any node in the tree is equal to the depth of its parent node plus 1.
+
+Write a function that takes in a Binary Tree and returns the sum of its node's depths.
+
+Each BinaryTree node has an integer value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None / null.
+
 
 //Solution 1 using Iterative approach
 class BinaryTree {
