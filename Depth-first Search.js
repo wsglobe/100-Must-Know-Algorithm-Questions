8acac9d9f176f4bb96bbfd13b4f377f0a70776ce
Walter Wang <wsglobe@gmail.com>
2020-11-17 17:30:33 -0600

    optimal complex

--- Depth-first Search.js
+++ Depth-first Search.js
@@ -1,5 +1,7 @@
-//Depth-first Search: navigate the tree from Left to Right, branch by branch, in other words, before traversing any node's sibling nodes, its children nodes must be traversed. (going deep before wide)
-//Solution
+/*Depth-first Search: navigate the tree from Left to Right, branch by branch, in other words, before traversing any node's sibling nodes, its children nodes must be traversed. (going deep before wide)
+*Optimal Space & Time Complexity
+O(v + e) time | O(v) space - where v is the number of vertices of the input graph and e is the number of edges of the input graph*/
+// Solution
 class Node {
   constructor(name) {
     this.name = name;
