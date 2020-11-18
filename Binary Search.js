ff04fc4b370e5013bad0892bef70621f0f204264
Walter Wang <wsglobe@gmail.com>
2020-11-17 21:06:20 -0600

    optimal complex

--- Binary Search.js
+++ Binary Search.js
@@ -1,7 +1,11 @@
-// Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1.
-// Sample Input [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
-// Target = 33
-// Sample Output 3
+/* Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1.
+
+Sample Input [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+Target = 33
+Sample Output 3
+
+*Optimal Space & Time Complexity
+O(log(n)) time | O(1) space - where n is the length of the input array */
 
 // Solution 1
 function binarySearch(array, target) {
