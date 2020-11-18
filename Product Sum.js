bb575a93e4bb297db8f526977dfd63b13e05cbf4
Walter Wang <wsglobe@gmail.com>
2020-11-17 20:10:02 -0600

    fix format

--- Product Sum.js
+++ Product Sum.js
@@ -18,6 +18,7 @@ O(n) time | O(d) space - where n is the total number of emelents in the array, i
 */
 
 // Solution
+
 function productSum(array, multiplier = 1) {
   let sum = 0;
   for (const element of array) {
