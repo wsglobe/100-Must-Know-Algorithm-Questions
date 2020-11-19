429edd823076d30ad554769aac62129fafcb3e70
Walter Wang <wsglobe@gmail.com>
2020-11-18 23:18:41 -0600

    optimal complex

--- Palindrome Check.js
+++ Palindrome Check.js
@@ -1,9 +1,17 @@
 /* Write a function that takes in a non-empty string and returns a boolean representing whether the string is a palindrome.
+
 A palidrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
+
 Sample Input:
-string = "abcdcba"; True */
+string = "abcdcba"
+Sample Output: 
+True
+
+*Optimal Space & Time Complexity
+
+O(n) time | O(1) space - where n is the length of the input string */
+
 // Solution
-// O(n) time | O(1) space
 
 function isPalindrome(string) {
   let leftIdx = 0;
