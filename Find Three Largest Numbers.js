65c69cbb02e7c464788c1124a66eb205a0d28b46
Walter Wang <wsglobe@gmail.com>
2020-11-18 21:55:07 -0600

    tip?

--- Find Three Largest Numbers.js
+++ Find Three Largest Numbers.js
@@ -1,9 +1,12 @@
 /* Write a function that takes in an array of at least three integers and, without sorting the input array, returns a sorted array of the three largest integers in the input array.
+
 Sample Input:
 array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
 Sample Output:
 [18, 141, 541]
+
 Tip: Can we just sort the input array and return the last three elements in the sorted array?
+
 
 // Solution
 function findThreeLargestNumbers(array) {
