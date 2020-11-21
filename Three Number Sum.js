3805b3de0637b45af3e7c415bd3faba7dacbe075
Walter Wang <wsglobe@gmail.com>
2020-11-20 21:18:44 -0600

    solution

--- Three Number Sum.js
+++ Three Number Sum.js
@@ -10,3 +10,28 @@ Sample Output:
 
 *Optimal Space & Time Complexity
 O(n^2) time | O(n) space - where n is the length of the input array */
+
+// Solution
+
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  const triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < targetSum) {
+        left++;
+      } else if (currentSum > targetSum) {
+        right--;
+      }
+    }
+  }
+  return triplets;
+}
+
