152ee55ff0860fa345bb22130ba288cb3f26cba6
Walter Wang <wsglobe@gmail.com>
2020-11-22 22:22:16 -0600

    sample

--- Smallest Difference.js
+++ Smallest Difference.js
@@ -3,3 +3,12 @@
 Note that the absolute difference of two integers is the distance between them on the real number line. For example, the absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
 
 You can assume that there will only be one pair of numbers with the smallest difference.
+
+Sample Inout:
+
+arrayOne = [-1, 5, 10, 20, 28, 3]
+arrayTwo = [26, 134, 135, 15, 17]
+
+Sample Output:
+
+[28, 26]
