01c6e41d1880c849210eb9cc4344fd1071d030e5
Walter Wang <wsglobe@gmail.com>
2020-11-25 21:22:53 -0600

    solution

--- Move Element To End.js
+++ Move Element To End.js
@@ -10,3 +10,23 @@ Note: the numbers 1, 3, 4 could be ordered differently
 
 O(n) time | O(1) space - where n is the length of the array */
 
+// Solution
+
+function moveElementToEnd(array, toMove) {
+  let i = 0;
+  let j = array.length - 1;
+  while(i < j) {
+    while (i < j && array[j] === toMove) j--;
+    if (array[i] === toMove) swap(i , j, array);
+    i++;
+  }
+  return array;
+}
+
+function swap(i ,j, array) {
+  const temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
+}
+
+exports.moveElementToEnd = moveElementToEnd;
