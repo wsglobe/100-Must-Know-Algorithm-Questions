a5f8d9eb001c9bd9c6b21868a89c497fbfbec225
Walter Wang <wsglobe@gmail.com>
2020-11-25 22:36:24 -0600

    s1

--- Monotonic Array.js
+++ Monotonic Array.js
@@ -14,3 +14,27 @@ Sample out: true
 *Optimal Space & Time Complexity
 
 O(n) Time | O(1) Space - where n is the length of the array */
+
+// Solution 1
+
+function isMonotonic(array) {
+  if (array.length <= 2) return true;
+
+  let direction = array[1] - array[0];
+  for (let i = 2; i < array.length; I++) {
+    if (direction === 0) {
+      direction = array[i] - array[i - 1];
+      continue;
+    }
+    if (breaksDirection(direction, array[i - 1], array [i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function breaksDirection(direction, previousInt, currentInt) {
+  const difference = currentInt - previousInt;
+  if (direction > 0) return difference < 0;
+  return difference > 0;
+}
