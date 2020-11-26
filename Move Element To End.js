b9d7f531c8ea04740f60f19454ffda3bcb4df8b7
Walter Wang <wsglobe@gmail.com>
2020-11-25 21:09:26 -0600

    sample

--- Move Element To End.js
+++ Move Element To End.js
@@ -1,2 +1,7 @@
 /* You're given an array of intergers and an integer. Write a function that moves all instances of that integer in the array to the end of the array and returns the array.
 
+Sample Input: 
+array = [2, 1, 2, 2, 2, 3, 4, 2] tomove = 2
+
+Sample Output: [1, 3, 4, 2, 2, 2, 2]
+Note: the numbers 1, 3, 4 could be ordered differently
