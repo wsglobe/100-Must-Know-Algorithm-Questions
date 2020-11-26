20d43cea9486e59c61730f9edeed0675d33b10e0
Walter Wang <wsglobe@gmail.com>
2020-11-25 21:57:30 -0600

    sample

--- Monotonic Array.js
+++ Monotonic Array.js
@@ -5,3 +5,12 @@ An array is said to be monotonic if its elements, from left to right, are entire
 Non-increasing elements aren't necessarily exclusively decreasing; they simply don't increase. Similarly, non-decreasing elements aren't necessarily exclusively increasing; they sinply don't decrease.
 
 Note that empty arrays and arrays of one element are monotonic.
+
+Sample Input:
+array = [-1, -5, -10, -1100, -1100, -1101, -1002, -9001]
+
+Sample out: true
+
+*Optimal Space & Time Complexity
+
+O(n) Time | O(1) Space - where n is the length of the array */
