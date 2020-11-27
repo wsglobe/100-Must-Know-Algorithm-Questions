e140b1f26cbef4839f75fc4c17a78d8eb1b1e8ca
Walter Wang <wsglobe@gmail.com>
2020-11-26 19:42:50 -0600

    question

---
+++ BST Construction.js
@@ -0,0 +1,5 @@
+/* Write a BST class for a Binary Search Tree. The class should support:
+- Inserting values with the insert method.
+- Removing values with the remove method; this method should only remove the first instance of a given value.
+- Searching for values with the contains method.
+Note that you can't remove values from a single-node tree. In other words, calling the remove method on a single-node tree should simply not do anything.
