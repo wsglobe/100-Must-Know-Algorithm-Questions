6119e586078424f9c5330bd9b3328511a558cc12
Walter Wang <wsglobe@gmail.com>
2020-11-26 21:11:31 -0600

    question

---
+++ Validate BST.js
@@ -0,0 +1,2 @@
+/* Write a function that takes in a potentially invalid BST and returns a boolean representing whether the BST is valid.
+A BST is valid if and only if all of its nodes are valid BST nodes.
