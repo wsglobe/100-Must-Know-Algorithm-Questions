d8650fa8ce780af1ea92c03bb7d598d748593d2d
Walter Wang <wsglobe@gmail.com>
2020-11-26 21:14:52 -0600

    optimal complex

--- Validate BST.js
+++ Validate BST.js
@@ -1,2 +1,5 @@
 /* Write a function that takes in a potentially invalid BST and returns a boolean representing whether the BST is valid.
 A BST is valid if and only if all of its nodes are valid BST nodes.
+
+*Optimal Space & Time Complexity
+O(n) Time | O(d) Space - where n is the number of nodes in the BST and d is the depth(height) of the BST.
