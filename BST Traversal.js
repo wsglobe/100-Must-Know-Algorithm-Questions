30fca2d2c4bbbe4ecd452e78ff4a7137a9e9a58a
Walter Wang <wsglobe@gmail.com>
2020-11-28 19:24:00 -0600

    optimal complex

--- BST Traversal.js
+++ BST Traversal.js
@@ -1,1 +1,5 @@
 /* Write three functions that take in a BST and an empty array, traverse the BST, add its nodes' value to the input array, and return that array. The three functions should traverse the BST using the in-order, pre-order, and post-order tree-traversal techniques, respectively.
+
+*Optimal Space & Time Complexity
+
+O(n) Time | O(n) Space - where n is the number of nodes in the BST
