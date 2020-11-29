9758f99b2021f73ea08db98a6a2ebe74e10624e7
Walter Wang <wsglobe@gmail.com>
2020-11-28 20:15:53 -0600

    hint 2

--- Min Height BST.js
+++ Min Height BST.js
@@ -3,3 +3,5 @@
 Hints: 
 
 In order for the BST to have the smallest height possible, it needs to be balanced; in other words, it needs to have roughly the same number of nodes in its left subtree as in its right subtree.
+
+Grab the middle element of the array, and make that element be the root node of the BST. Then, grab the middle element between the begining of the array and the first middle element, and make that element be the root of the BST's left subtree; similarly, make the middle element between the end of the array and the first middle element be the root of the BST's right subtree. Continue this approach until you run out of elements in the array.
