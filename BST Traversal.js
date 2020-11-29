d3443f06aa191006e76ea73cfbbc17e17dbd9bb8
Walter Wang <wsglobe@gmail.com>
2020-11-28 19:21:30 -0600

    des

--- BST Traversal.js
+++ BST Traversal.js
@@ -1,1 +1,1 @@
-/*
+/* Write three functions that take in a BST and an empty array, traverse the BST, add its nodes' value to the input array, and return that array. The three functions should traverse the BST using the in-order, pre-order, and post-order tree-traversal techniques, respectively.
