f57e89712448e58e007b866766b475448d299e16
Walter Wang <wsglobe@gmail.com>
2020-11-28 20:17:03 -0600

    optimal complex

--- Min Height BST.js
+++ Min Height BST.js
@@ -5,3 +5,7 @@ Hints:
 In order for the BST to have the smallest height possible, it needs to be balanced; in other words, it needs to have roughly the same number of nodes in its left subtree as in its right subtree.
 
 Grab the middle element of the array, and make that element be the root node of the BST. Then, grab the middle element between the begining of the array and the first middle element, and make that element be the root of the BST's left subtree; similarly, make the middle element between the end of the array and the first middle element be the root of the BST's right subtree. Continue this approach until you run out of elements in the array.
+
+*Optimal Space & Time Complexity
+
+O(n) Time | O(n) Space - where n is the length of the array */
