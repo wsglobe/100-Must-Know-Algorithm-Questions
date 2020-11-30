5d1028dee3848898d92ca7ecad10b8dbdb9a8bd3
Walter Wang <wsglobe@gmail.com>
2020-11-29 22:29:15 -0600

    s1

--- Invert Binary Tree.js
+++ Invert Binary Tree.js
@@ -2,4 +2,35 @@
 
 *Optimal Space & Time Complexity
 
-O(n) Time | O(d) Space - where n is the number of nodes in the Binary Tree and d is the depth(height) of the Binary Tree.
+O(n) Time | O(d) Space - where n is the number of nodes in the Binary Tree and d is the depth(height) of the Binary Tree. */
+
+// Solution 1
+// O(n) Time | O(n) Space
+
+function invertBinaryTree(tree) {
+  const queue = [tree];
+  while (queue.length) {
+    const current = queue.shift();
+    if (current === null) continue;
+    swapLeftAndRight(current);
+    queue.push(current.left);
+    queue.push(current.right);
+  }
+}
+
+function swapLeftAndRight(tree) {
+  const left = tree.left;
+  tree.left = tree.right;
+  tree.right = left;
+}
+
+// Solution 2
+// O(n) Time | O(d) Space
+
+funciton invertBinaryTree(tree) {
+  if (tree === null) return;
+  swapLeftAndRight(tree);
+  invertBinaryTree(tree.left);
+  invertBinaryTree(tree.right);
+}
+
