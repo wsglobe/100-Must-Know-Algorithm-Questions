e18095a1ba726ec0823daccc1ebd710f0bbe9f11
Walter Wang <wsglobe@gmail.com>
2020-11-30 11:22:28 -0600

    s1

--- Max Subset Sum No Adjacent.js
+++ Max Subset Sum No Adjacent.js
@@ -10,4 +10,18 @@ Sample Output:
 
 *Optimal Space & Time Complexity
 
-O(n) Time | O(1) Space - where n is the length of the input array
+O(n) Time | O(1) Space - where n is the length of the input array */
+
+// Solution 1
+// O(n) Time | O(n) Space
+
+function maxSubsetSumNoAdjacent(array) {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+  const maxSums = array.slice();
+  maxSums[1] = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    maxSums[i] = Math.max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+  }
+  return maxSums[maxSums.length - 1];
+}
