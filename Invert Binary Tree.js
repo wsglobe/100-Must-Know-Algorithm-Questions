e5976679f7e879b119c8bf35906b94ff9a7a5e57
Walter Wang <wsglobe@gmail.com>
2020-11-29 21:51:31 -0600

    optimal complex

--- Invert Binary Tree.js
+++ Invert Binary Tree.js
@@ -1,1 +1,5 @@
 /* Write a function that takes in a Binary Tree and inverts it. In other words, the function should swap every left node in the tree for its corresponding right node.
+
+*Optimal Space & Time Complexity
+
+O(n) Time | O(d) Space - where n is the number of nodes in the Binary Tree and d is the depth(height) of the Binary Tree.
