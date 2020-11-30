8602d84e18164767eed933426c39e90eaa458e7e
Walter Wang <wsglobe@gmail.com>
2020-11-29 22:58:48 -0600

    des

---
+++ Max Subset Sum No Adjacent.js
@@ -0,0 +1,3 @@
+/* Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
+
+If the input array is empty, the function should return 0.
