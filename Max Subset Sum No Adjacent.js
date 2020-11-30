de096375999e2b65abf86fc290a84e41dd0420dc
Walter Wang <wsglobe@gmail.com>
2020-11-30 11:25:50 -0600

    s2

--- Max Subset Sum No Adjacent.js
+++ Max Subset Sum No Adjacent.js
@@ -25,3 +25,19 @@ function maxSubsetSumNoAdjacent(array) {
   }
   return maxSums[maxSums.length - 1];
 }
+
+// Solution 2
+// O(n) Time | O(1) Space
+
+function maxSubsetSumNoAdjacent(array) {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    const current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+  return first;
+}
