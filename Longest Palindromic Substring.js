0c235e71d8ce5740d9b1d678a828e3c8714b9141
Walter Wang <wsglobe@gmail.com>
2020-12-02 01:31:18 -0600

    sample

--- Longest Palindromic Substring.js
+++ Longest Palindromic Substring.js
@@ -3,3 +3,7 @@
 A palindrome is defined as a string that's written the same forward and backward. Note that single-character strings are parlindromes.
 
 You can assume that there will only be one longest palindromic substring.
+
+Sample Input: string = "abaxyzzyxf"
+
+Sample Output: "xyzzyx"
