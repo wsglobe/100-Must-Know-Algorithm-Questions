677f6ebe3ee7a999d71dc36a49a0427981953085
Walter Wang <wsglobe@gmail.com>
2020-12-02 01:29:33 -0600

    question

---
+++ Longest Palindromic Substring.js
@@ -0,0 +1,5 @@
+/* Write a function that, given a string, returns its longest palindromic substring.
+
+A palindrome is defined as a string that's written the same forward and backward. Note that single-character strings are parlindromes.
+
+You can assume that there will only be one longest palindromic substring.
