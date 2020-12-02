b2e8d68998ecb62f3bddaa350471c36b70fb55b2
Walter Wang <wsglobe@gmail.com>
2020-12-01 23:08:03 -0600

    s1

--- Breadth-first Search.js
+++ Breadth-first Search.js
@@ -2,4 +2,30 @@
 
 *Optimal Space & Time Complexity
 
-O(v + e) Time | O(v) Space - where v is the number of vertices of the input graph and e is the number of edges of the input graph
+O(v + e) Time | O(v) Space - where v is the number of vertices of the input graph and e is the number of edges of the input graph */
+
+// Solution
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  breadthFirstSearch(array) {
+    const queue = [this];
+    while (queue.length > 0) {
+      const current = queue.shift();
+      array.push(current.name);
+      for (const child of current.children) {
+        queue.push(child);
+      }
+    }
+    return array;
+  }
+}
