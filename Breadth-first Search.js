062bb0aa48adff3b3069d4cc10b6886650fb5690
Walter Wang <wsglobe@gmail.com>
2020-12-01 22:44:23 -0600

    optimal complex

--- Breadth-first Search.js
+++ Breadth-first Search.js
@@ -1,1 +1,5 @@
 /* The Breadth-first Search algorithm works by traversing a graph level by level. In other words, before traversing any Node's children Nodes, its sibling nodes must be traversed.
+
+*Optimal Space & Time Complexity
+
+O(v + e) Time | O(v) Space - where v is the number of vertices of the input graph and e is the number of edges of the input graph
