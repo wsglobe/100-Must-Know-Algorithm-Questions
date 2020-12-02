81766afbb82c1da7716e0c9e6e526025ce04c2a7
Walter Wang <wsglobe@gmail.com>
2020-12-02 12:16:08 -0600

    s1

--- Longest Palindromic Substring.js
+++ Longest Palindromic Substring.js
@@ -12,4 +12,26 @@ Hint: Recognize that a palindrome is a string that is symmetrical with respect t
 
 *Optimal Space & Time Complexity
 
-O(n^2) Time | O(n) Space - where n is the length of the input string
+O(n^2) Time | O(n) Space - where n is the length of the input string */
+
+// Solution
+
+function longestPalindromicSubstring(string) {
+  let currentLongest = [0, 1];
+  for (let i = 1; i < string.length; i++) {
+    const odd = getLongestPalindromeFrom(string, i - 1,i + 1);
+    const even = getLongestPalindromeFrom(string, i - 1, i);
+    const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    currentLongest = currentLongest[1] - currentLongest[0] > longest[1] - longest[0] ? currentLongest : longest;
+  }
+  return string.slice(currentLongest[0], currentLongest[1]);
+}
+
+function getLongestPalindromeFrom(string, leftIdx, rightIdx) {
+  while (leftIdx >= 0 && rightIdx < string.length) {
+    if (string[leftIdx] !== string[rightIdx]) break;
+    leftIdx--;
+    rightIdx++;
+  }
+  return [leftIdx + 1, rightIdx];
+}
