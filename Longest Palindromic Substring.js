626c4a4e0922352833aa6067ab817c7a1175631a
Walter Wang <wsglobe@gmail.com>
2020-12-02 01:37:18 -0600

    hint

--- Longest Palindromic Substring.js
+++ Longest Palindromic Substring.js
@@ -7,3 +7,5 @@ You can assume that there will only be one longest palindromic substring.
 Sample Input: string = "abaxyzzyxf"
 
 Sample Output: "xyzzyx"
+
+Hint: Recognize that a palindrome is a string that is symmetrical with respect to its center, which can either be a character (in the case of odd-length palindromes) or an empty string (in the case of even-length palindromes.) Thus, you can check the parlindromicity of a string by simply expanding from its center and making sure that characters on both sides are indeed mirrored.
