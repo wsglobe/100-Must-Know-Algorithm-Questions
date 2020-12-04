13d0d3ce12f3f98a788d7e341b3908833d75ab70
Walter Wang <wsglobe@gmail.com>
2020-12-03 19:53:11 -0600

    solution

--- Single Cycle Check.js
+++ Single Cycle Check.js
@@ -10,4 +10,23 @@ Sample Output: true
 
 *Optimal Space & Time Complexity
 
-O(n) Time | O(1) Space - where n is the length of the input array
+O(n) Time | O(1) Space - where n is the length of the input array */
+
+// Solution
+
+function hasSingleCycle(array) {
+  let numElementsVisited = 0;
+  let currentIdx = 0;
+  while (numElementsVisited < array.length) {
+    if (numElementsVisited > 0 && currentIdx === 0) return false;
+    numElementsVisited++;
+    currentIdx = getNextIdx(currentIdx, array);
+  }
+  return currentIdx === 0;
+}
+
+function getNextIdx(currentIdx, array) {
+  const jump = array[currentIdx];
+  const nextIdx = (currentIdx + jump) % array.length;
+  return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}
