4f251ea99e792298c8f9bc2975e4770e48a01a74
Walter Wang <wsglobe@gmail.com>
2020-12-03 19:26:13 -0600

    sample

--- Single Cycle Check.js
+++ Single Cycle Check.js
@@ -3,3 +3,7 @@
 If a jump spills past the array's bounds, it wraps over to the other side. For instance, a jump of -1 at index 0 bring us to the last index in the array. Similarly, a jump of 1 at the last index in the array brings us to index 0.
 
 Write a function that returns a boolean representing whether the jummps in the array form a single cycle. A single cycle occurs if, starting at any index in the array and following the jumps, every element in the array is visited exactly once before landing back on the starting index.
+
+Sample Input: array = [2, 3, 1, -4, -4, 2]
+
+Sample Output: true
