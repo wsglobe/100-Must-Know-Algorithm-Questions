6444c3373c0f4698a85f49e8cb34a6dc9c1f04b2
Walter Wang <wsglobe@gmail.com>
2020-12-05 20:33:45 -0600

    s2

--- Array  of products.js
+++ Array  of products.js
@@ -29,3 +29,29 @@ function arrayOfProducts(array) {
   }
   return products;
 }
+
+// Solution 2
+// O(n) Time | O(n) Space
+
+function arrayOfProducts(array) {
+  const products = new Array(array.length).fill(1);
+  const leftProducts = new Array(array.length).fill(1);
+  const rightProducts = new Array(array.length).fill(1);
+
+  let leftRunningProduct = 1;
+  for (let i = 0; i < array.length; i++) {
+    leftProducts[i] = leftRunningProduct;
+    leftRunningProduct *= array[i];
+}
+
+  let rightRunningProduct = 1;
+  for (let i = array.length - 1; i > -1; i--) {
+    rightProducts[i] = rightRunningProduct;
+    rightRunningProduct *= array[i];
+}
+
+  for (let i = 0; i < array.length; i++) {
+    products[i] = leftProducts[i] * rightProducts[i];
+  }
+  return products;
+}
