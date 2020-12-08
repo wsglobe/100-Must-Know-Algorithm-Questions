7727686affa9ba1f4aa663b87a88b0340142bca2
Walter Wang <wsglobe@gmail.com>
2020-12-07 20:22:15 -0600

    s1

--- Permutations.js
+++ Permutations.js
@@ -8,4 +8,24 @@ Sample Output: [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
 
 *Optimal Space & Time Complexity
 
-O(n*n!) Time | O(n*n!) Space - where n is the length of the input array
+O(n*n!) Time | O(n*n!) Space - where n is the length of the input array */
+
+// Solution 1
+
+function getPermutations(array) {
+  const permutations = [];
+  permutationsHelper(array, [], permutations);
+  return permutations;
+}
+
+function permutationsHelper(array, currentPermutation, permutations) {
+  if (!array.length && currentPermutation.length) {
+    permutations.push(currentPermutation);
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      const newArray = array.slice(0, i).concat(array.slice(i + 1));
+      const newPermutation = currentPermutation.concat([array[i]]);
+      permutationsHelper(newArray, newPermutation, permutations);
+    }
+  }
+}
