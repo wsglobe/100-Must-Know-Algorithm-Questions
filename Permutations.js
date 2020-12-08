e1e27f46daf095b2f794be37fd52186a7b43c3d1
Walter Wang <wsglobe@gmail.com>
2020-12-07 19:45:15 -0600

    sample input

--- Permutations.js
+++ Permutations.js
@@ -1,3 +1,5 @@
 /* Write a function that takes in an array of unique integers and returns an array of all permutations of those integers in no particular order.
 
 If the input array is empty, the function should return an empty array.
+
+Sample Input: array = [1,2,3]
