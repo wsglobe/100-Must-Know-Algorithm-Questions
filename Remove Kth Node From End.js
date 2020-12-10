3746767a2a7e50d78b331250d4b31bea5605edb5
Walter Wang <wsglobe@gmail.com>
2020-12-09 19:05:53 -0600

    solution

--- Remove Kth Node From End.js
+++ Remove Kth Node From End.js
@@ -22,3 +22,32 @@ Sample Output:
 
 O(n) Time | O(1) Space - where n is the number of nodes in the Linked List */
 
+// Solution
+
+class linkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeKthNodeFromEnd(head, k) {
+  let counter = 1;
+  let first = head;
+  let second = head;
+  while (counter <= k) {
+    second = second.next;
+    counter++;
+  }
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+  first.next = first.next.next;
+}
+
