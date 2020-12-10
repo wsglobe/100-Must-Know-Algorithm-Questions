6d32fb77eb8ce823c2f15927f07dc53c624872a5
Walter Wang <wsglobe@gmail.com>
2020-12-09 18:33:15 -0600

    sample

--- Remove Kth Node From End.js
+++ Remove Kth Node From End.js
@@ -9,3 +9,11 @@ Note that your function doesn't need to return anything.
 You can assume that the input Linked List will always have at least two nodes and, more specifically, at least k nodes.
 
 Each linkedList node has an integer value as well as a next node pointing to the next node in the list or to None/null if it's the tail of the list.
+
+Sample Input:
+head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9      k = 4
+
+Sample Output:
+// No output required.
+// The 4th node from the end of the lsit(the node with value 6) is removed.
+0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9 */
