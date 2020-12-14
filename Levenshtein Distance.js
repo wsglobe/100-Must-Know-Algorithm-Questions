28be3aa64a9757d30d6e9641bc4343e3961fe15e
Walter Wang <wsglobe@gmail.com>
2020-12-13 19:54:59 -0600

    s2

--- Levenshtein Distance.js
+++ Levenshtein Distance.js
@@ -18,7 +18,7 @@ function levenshteinDistance(str1, str2) {
   for (let i = 0; i < str2.length + 1; i++) {
     const row = [];
     for (let j = 0; j < str1.length + 1; j++) {
-      row.push{j};
+      row.push(j);
     }
     row[0] = i;
     edits.push(row);
@@ -33,4 +33,36 @@ function levenshteinDistance(str1, str2) {
     }
   }
   return edits[str2.length][str1.length];
+}
+
+// Solution 2
+// O(nm) Time | O(min(n, m)) Space
+
+function levenshteinDistance(str1, str2) {
+  const small = str1.length < str2.length ? str1 : str2;
+  const big = str1.length >= str2.length ? str1 : str2;
+  const evenEdits = [];
+  const oddEdits = new Array(small.length + 1);
+  for (let j = 0; j < small.length + 1; j++) {
+    evenEdits.push(j);
+  }
+  for (let i = 1; i < big.length + 1; i++) {
+    let currentEdits, previousEdits;
+    if (i % 2 === 1) {
+      currentEdits = oddEdits;
+      previousEdits = evenEdits;
+    } else {
+      currentEdits = evenEdits;
+      previousEdits = oddEdits;
+    }
+    currentEdits[0] = 1;
+    for (let j = 1; j < small.length + 1; j++) {
+      if (big[i - 1] === small[j - 1]) {
+        currentEdits[j] = previousEdits[j - 1];
+      } else {
+        currentEdits[j] = 1 + Math.min(previousEdits[j - 1], previousEdits[j], currentEdits[j - 1]);
+      }
+    }
+  }
+  return big.length % 2 === 0 ? evenEdits[small.length] : oddEdits[small.length];
 }
