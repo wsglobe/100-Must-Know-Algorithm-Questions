d15d894fad1d25becceb6c952191fc5f3719746c
Walter Wang <wsglobe@gmail.com>
2020-12-13 19:40:17 -0600

    s1

--- Levenshtein Distance.js
+++ Levenshtein Distance.js
@@ -10,4 +10,27 @@ Sample Output: 2 // insert "y"; substitute "c" for "d"
 
 O(nm) Time | O(min(n.m)) Space - where n and m are the lengths of the two input strings */
 
-// Solution
+// Solution 1
+// O(nm) Time | O(nm) Space
+
+function levenshteinDistance(str1, str2) {
+  const edits = [];
+  for (let i = 0; i < str2.length + 1; i++) {
+    const row = [];
+    for (let j = 0; j < str1.length + 1; j++) {
+      row.push{j};
+    }
+    row[0] = i;
+    edits.push(row);
+  }
+  for (let i = 1; i < str2.length + 1; i++) {
+    for (let j = 1; j < str1.length + 1; j++) {
+      if (str2[i - 1] === str1[j - 1]) {
+        edits[i][j] = edits[i - 1][j - 1];
+      } else {
+        edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1]);
+      }
+    }
+  }
+  return edits[str2.length][str1.length];
+}
