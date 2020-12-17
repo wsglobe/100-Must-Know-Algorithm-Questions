5eb8bf8bc8caf65ec0c715adf5f9d796d601e5b5
Walter Wang <wsglobe@gmail.com>
2020-12-16 19:16:04 -0600

    solution

--- Kadane's Algorithm.js
+++ Kadane's Algorithm.js
@@ -6,4 +6,17 @@ Sample Output: 19 // [1,3,-2,3,4,7,2,-9,6,3,1]
 
 *Optimal Space & Time Complexity
 
-O(n) Time | O(1) Space - where n is the length of the input array
+O(n) Time | O(1) Space - where n is the length of the input array */
+
+// Solution
+
+function kadanesAlgorithm(array) {
+  let maxEndingHere = array[0];
+  let maxSoFar = array[0];
+  for (let i = 1; i < array.length; i++) {
+    const num = array[i];
+    maxEndingHere = Math.max(num, maxEndingHere + num);
+    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+  }
+  return maxSoFar;
+}
