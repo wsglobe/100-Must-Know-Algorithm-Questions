5199e54c00854b3b6c3daa4f9902bea5e4836095
Walter Wang <wsglobe@gmail.com>
2020-12-19 17:40:02 -0600

    question

---
+++ Group Anagrams.js
@@ -0,0 +1,5 @@
+/* Write a function that takes in an array of strings and groups anagrams together.
+
+Anagrams are strings made up of exactly the same letters, where order doesn't matter. For example, "cinema" and "iceman" are anagrams; similarly, "foo" and "ofo" are anagrams.
+
+Your function should return a list of anagram group in no particular order.
