d35eb015108124687568d50247276aeb184ef44e
Walter Wang <wsglobe@gmail.com>
2020-12-20 16:31:07 -0600

    sample

--- Group Anagrams.js
+++ Group Anagrams.js
@@ -3,3 +3,9 @@
 Anagrams are strings made up of exactly the same letters, where order doesn't matter. For example, "cinema" and "iceman" are anagrams; similarly, "foo" and "ofo" are anagrams.
 
 Your function should return a list of anagram group in no particular order.
+
+Sample Input:
+words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+
+Sample Output:
+[["yo","oy"], ["flop","olfp"], ["act","tac","cat"],["foo"]]
